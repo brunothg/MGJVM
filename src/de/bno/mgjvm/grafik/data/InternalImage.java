416a868edecfbc473f7687c1a5fb7abf77f67dfb
Marvin Bruns <Marvin Bruns@n-marvin>
2014-06-22 20:52:34 +0200

    InternalImage
    Chached und synchronized

--- src/de/bno/mgjvm/grafik/data/InternalImage.java
+++ src/de/bno/mgjvm/grafik/data/InternalImage.java
@@ -2,32 +2,81 @@ package de.bno.mgjvm.grafik.data;
 
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 
 public class InternalImage {
 
-	public static Image load(String s) {
-		return Toolkit.getDefaultToolkit().getImage(
-				InternalImage.class.getResource("/de/bno/mgjvm/grafik/data/"
-						+ s));
+	static Map<String, Image> loadedImages;
+
+	static {
+		loadedImages = new HashMap<String, Image>();
 	}
 
-	public static Image fullLoad(String s) throws IOException {
-		return ImageIO.read(InternalImage.class
+	public static Image load(String s) {
+
+		try {
+			return fullLoad(s);
+		} catch (IOException e) {
+		}
+
+		return null;
+	}
+
+	public static Image reloadFull(String s) throws IOException {
+		Image ret;
+
+		ret = ImageIO.read(InternalImage.class
 				.getResource("/de/bno/mgjvm/grafik/data/" + s));
+
+		synchronized (loadedImages) {
+			loadedImages.put(s, ret);
+		}
+
+		return ret;
+
+	}
+
+	private static Image fullLoad(String s) throws IOException {
+
+		Image ret;
+
+		synchronized (loadedImages) {
+			ret = loadedImages.get(s);
+		}
+
+		if (ret != null) {
+			return ret;
+		}
+
+		ret = ImageIO.read(InternalImage.class
+				.getResource("/de/bno/mgjvm/grafik/data/" + s));
+
+		synchronized (loadedImages) {
+			loadedImages.put(s, ret);
+		}
+
+		return ret;
 	}
 
 	public static Image load(String s, int width, int height) {
 		Image img;
-		try {
-			img = fullLoad(s);
-		} catch (IOException e) {
-			return load(s);
+
+		synchronized (loadedImages) {
+			img = loadedImages.get(s);
+		}
+
+		if (img == null) {
+			try {
+				img = fullLoad(s);
+			} catch (IOException e) {
+				return load(s);
+			}
 		}
 
 		BufferedImage ret = new BufferedImage(width, height,
