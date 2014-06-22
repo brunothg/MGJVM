66ed68a95846d8f3da07ccb247c099f4f718a1d4
brunothg <android.bruns@googlemail.com>
2014-06-22 15:09:40 +0200

    LookAndFeel
    Nimbus alternative

--- src/de/bno/mgjvm/MGJVM.java
+++ src/de/bno/mgjvm/MGJVM.java
@@ -3,6 +3,7 @@ package de.bno.mgjvm;
 import java.io.File;
 
 import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
 
 import de.bno.mgjvm.grafik.GrafischeJVM;
 
@@ -26,11 +27,33 @@ public class MGJVM {
 	}
 
 	private static void setLookAndFeel() {
+
+		String system = getSystemLookAndFeel();
+		String nimbus = getNimbusLookAndFeel();
+
 		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			UIManager.setLookAndFeel(system);
 		} catch (Exception e) {
-			e.printStackTrace();
+			try {
+				UIManager.setLookAndFeel(nimbus);
+			} catch (Exception e1) {
+			}
 		}
+	}
+
+	private static String getSystemLookAndFeel() {
+
+		return UIManager.getSystemLookAndFeelClassName();
+	}
+
+	private static String getNimbusLookAndFeel() {
+		for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+			if ("Nimbus".equals(info.getName())) {
+				return (info.getClassName());
+			}
+		}
+
+		return null;
 	}
 
 }
