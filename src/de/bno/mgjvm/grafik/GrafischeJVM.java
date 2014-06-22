1be6bccc0f834ec63e06306968c39926b86af64c
Marvin Bruns <Marvin Bruns@n-marvin>
2014-06-22 23:46:34 +0200

    GrafischeJVM
    ProgramCounter vorerst zumindest aus dem Editor Bereich entfernt

--- src/de/bno/mgjvm/grafik/GrafischeJVM.java
+++ src/de/bno/mgjvm/grafik/GrafischeJVM.java
@@ -8,17 +8,13 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.border.EmptyBorder;
 
 import de.bno.mgjvm.data.Open;
 import de.bno.mgjvm.grafik.data.InternalImage;
-import javax.swing.JSplitPane;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
 import javax.swing.border.TitledBorder;
-import javax.swing.UIManager;
 
 public class GrafischeJVM extends JFrame implements SaveListener, OpenListener,
 		ExecutionListener {
@@ -77,13 +73,7 @@ public class GrafischeJVM extends JFrame implements SaveListener, OpenListener,
 		editorScrollPane.setViewportView(editor);
 
 		JPanel tablePanel = new JPanel();
-		GridBagLayout gbl_tablePanel = new GridBagLayout();
-		gbl_tablePanel.columnWidths = new int[] { 0, 0 };
-		gbl_tablePanel.rowHeights = new int[] { 0, 0, 0, 0 };
-		gbl_tablePanel.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
-		gbl_tablePanel.rowWeights = new double[] { 0.0, 1.0, 1.0,
-				Double.MIN_VALUE };
-		tablePanel.setLayout(gbl_tablePanel);
+		tablePanel.setLayout(new BoxLayout(tablePanel, BoxLayout.Y_AXIS));
 
 		splitPane = new JSplitPane();
 		splitPane.setDividerSize(10);
@@ -91,36 +81,17 @@ public class GrafischeJVM extends JFrame implements SaveListener, OpenListener,
 		splitPane.setResizeWeight(1.0);
 		splitPane.setLeftComponent(editorScrollPane);
 		splitPane.setRightComponent(tablePanel);
-		contentPane.add(splitPane, BorderLayout.CENTER);
-
-		ProgramCounter programCounter = new ProgramCounter();
-		GridBagConstraints gbc_programCounter = new GridBagConstraints();
-		gbc_programCounter.anchor = GridBagConstraints.NORTH;
-		gbc_programCounter.insets = new Insets(0, 0, 5, 0);
-		gbc_programCounter.fill = GridBagConstraints.HORIZONTAL;
-		gbc_programCounter.gridx = 0;
-		gbc_programCounter.gridy = 0;
-		tablePanel.add(programCounter, gbc_programCounter);
 
 		ConstantPool constantPool = new ConstantPool();
-		constantPool.setBorder(new TitledBorder(UIManager
-				.getBorder("TitledBorder.border"), "Constant Pool",
+		constantPool.setBorder(new TitledBorder(null, "Constant Pool",
 				TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		GridBagConstraints gbc_constantPool = new GridBagConstraints();
-		gbc_constantPool.insets = new Insets(0, 0, 5, 0);
-		gbc_constantPool.fill = GridBagConstraints.BOTH;
-		gbc_constantPool.gridx = 0;
-		gbc_constantPool.gridy = 1;
-		tablePanel.add(constantPool, gbc_constantPool);
+		tablePanel.add(constantPool);
 
 		FieldPool fieldPool = new FieldPool();
 		fieldPool.setBorder(new TitledBorder(null, "Field Pool",
 				TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		GridBagConstraints gbc_fieldPool = new GridBagConstraints();
-		gbc_fieldPool.fill = GridBagConstraints.BOTH;
-		gbc_fieldPool.gridx = 0;
-		gbc_fieldPool.gridy = 2;
-		tablePanel.add(fieldPool, gbc_fieldPool);
+		tablePanel.add(fieldPool);
+		contentPane.add(splitPane, BorderLayout.CENTER);
 
 		menuBar = new BMenuBar();
 		menuBar.setSaveListener(this);
